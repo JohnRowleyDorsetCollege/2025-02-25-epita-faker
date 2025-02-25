62541779ff82c1e120d526c1134b42a7f2fa854b
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-02-25 09:24:29 +0000

    Added Faker

--- Services/GenerateUserProfiles.cs
+++ Services/GenerateUserProfiles.cs
@@ -8,16 +8,17 @@ namespace _2025_02_25_epita_faker.Services
 
         public static List<UserProfile> GenerateProfiles(int count)
         {
+           
             List<UserProfile> userProfiles = new List<UserProfile>();
             for (int i = 0; i < count; i++)
             {
                 userProfiles.Add(new UserProfile
                 {
-                    //    Name = Faker.Name.FullName(),
-                    //    GovernmentId = Faker.Identification.SocialSecurityNumber(),
-                    //    Followers = Faker.RandomNumber.Next(0, 1000000),
-                    //    Location = Faker.Address.City(),
-                    //    Biography = Faker.Lorem.Paragraph()
+                    Name = Faker.Name.FullName(),
+                    GovernmentId = Faker.Identification.UKNationalInsuranceNumber(),
+                    Followers = Faker.RandomNumber.Next(0, 1000000),
+                    Location = Faker.Address.City(),
+                    Biography = Faker.Lorem.Paragraph()
                 });
             }
             return userProfiles;
